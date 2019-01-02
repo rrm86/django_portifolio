5eef78ca9aaf950b3a20ed1f3bc9df6efd6db8ae
Ronnald R Machado <rrmachado@id.uff.br>
2019-01-02 20:55:31 -0200

    Create blog Models and add to admin

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,8 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.summary
+    
+
   
