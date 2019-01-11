4e02f6442613ce73be96ff1cb213c4617ba17eec
Ronnald R Machado <rrmachado@id.uff.br>
2019-01-11 16:11:15 -0200

    Add portfolio page and work with static files

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def portfolio_list(request):
+    jobs = Job.objects
+    return render(request, 'jobs/portfolio.html',{'jobs':jobs})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',jobs.views.home, name='home')
+    path('',jobs.views.home, name='home'),
+    path('portfolio/',jobs.views.portfolio_list, name='portfolio')
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
