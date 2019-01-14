4deb5e1d230bd446972177728f28429bcab6e984
Ronnald R Machado <rrmachado@id.uff.br>
2019-01-14 11:57:44 -0200

    Change the jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class Job(models.Model):
+    title = models.CharField(default='My title',max_length=50)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    fulltext = models.TextField(default='My full text')
+    show =  models.BooleanField(default=1)
 
     def __str__(self):
-        return self.summary
+        return self.title
     
 
   
